caab5a640db296d1a2591f47d4247a37d98554e4
ngoctrong2014 <xanhnguyen.18011988@gmail.com>
2025-04-04 17:40:57 +0700

    Create Clock.java

---
+++ Clock.java
@@ -0,0 +1,6 @@
+git clone https://github.com/ngoctrong2014/ngoctrong2014.md.git
+cd ngoctrong2014.md
+mv "đồng hồ" Clock.java  # Đổi tên file nếu cần
+git add Clock.java
+git commit -m "Thêm code đồng hồ Java"
+git push origin main
